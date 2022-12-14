97f1cc34600fb808f17157f6f37a8573f3307fe4
DaveyJH <daveyj.dh@googlemail.com>
2022-12-14 01:19:13 +0000

    feat: test ip retrieval

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+import os
+
+if "USERIP" in os.environ:
+    print(os.environ["USERIP"])
+
+print("tested")
